2f8a3b055d93ac6a6b83ed8d414fe07e6461560c
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-07 19:43:31 +0200

    Add Conditional Statement Advance

---
+++ Programming Basics C#/03.Conditional-Statement-Advance/Lab/T04.PersonalTitles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace T04.PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            switch (gender)
+            {
+                case "f":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Miss");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                    break;
+                case "m":
+                    if (age < 16)
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mr.");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Programming Basics C#/03.Conditional-Statement-Advance/Lab/T07.WorkingHours/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace T07.WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (time >= 10 && time <= 18)
+            {
+                switch (day)
+                {
+                    case "Monday":
+                    case "Tuesday":
+                    case "Wednesday":
+                    case "Thursday":
+                    case "Friday":
+                    case "Saturday":
+                        Console.WriteLine("open");
+                        break;
+                    case "Sunday":
+                        Console.WriteLine("closed");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+            
+
+        }
+    }
+}
