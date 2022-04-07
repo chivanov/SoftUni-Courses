792d46bc81aa681637e65ea821fd283a750481a1
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-07 21:04:24 +0200

    Update T06 in Conditional Statement

--- Programming Basics C#/02.Conditional-Statements/Lab/T06.SpeedInfo/Program.cs
+++ Programming Basics C#/02.Conditional-Statements/Lab/T06.SpeedInfo/Program.cs
@@ -6,11 +6,7 @@ namespace T06.SpeedInfo
     {
         static void Main(string[] args)
         {
-            // •	При скорост до 10(включително) отпечатайте "slow"
-            //•	При скорост над 10 и до 50(включително) отпечатайте "average"
-            //•	При скорост над 50 и до 150(включително) отпечатайте "fast"
-            //•	При скорост над 150 и до 1000(включително) отпечатайте "ultra fast"
-            //•	При по-висока скорост отпечатайте "extremely fast"
+           
             double speed = double.Parse(Console.ReadLine());
 
             if (speed <= 10)
