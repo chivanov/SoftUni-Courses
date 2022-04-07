63277a4d14f16a045c60fdce4ee61c402c695c8f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-07 10:09:37 +0200

    Update Program.cs

--- Programming Basics C#/01.First-Steps-In-Coding/Exercise/T03DepositCalculator/Program.cs
+++ Programming Basics C#/01.First-Steps-In-Coding/Exercise/T03DepositCalculator/Program.cs
@@ -6,9 +6,7 @@ namespace T03DepositCalculator
     {
         static void Main(string[] args)
         {
-            //1.Депозирана сума – реално число в интервала[100.00 … 10000.00]
-            //2.Срок на депозита(в месеци) – цяло число в интервала[1…12]
-            //3.Годишен лихвен процент – реално число в интервала[0.00 …100.00]
+          
             double deposit = double.Parse(Console.ReadLine());
             int depositPeriod = int.Parse(Console.ReadLine());
             double anualInterest = double.Parse(Console.ReadLine());
