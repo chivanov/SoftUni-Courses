426c231f4225647290cf8d8c41fb261625927a0c
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-07 09:39:46 +0200

    Update Program.cs

--- Programming Basics C#/01.First-Steps-In-Coding/Exercise/Fishland/Program.cs
+++ Programming Basics C#/01.First-Steps-In-Coding/Exercise/Fishland/Program.cs
@@ -5,12 +5,7 @@ namespace Fishland
     class Program
     {
         static void Main(string[] args)
-        {
-            //•	Първи ред – цена на скумрията на килограм.Реално число в интервала[0.00…40.00]
-            //•	Втори ред – цена на цацата на килограм.Реално число в интервала[0.00…30.00]
-            //•	Трети ред – килограма паламуд. Реално число в интервала[0.00…50.00]
-            //•	Четвърти ред – килограма сафрид. Реално число в интервала[0.00… 70.00]
-            //•	Пети ред – килограма миди. Цяло число в интервала[0... 100]
+        {        
 
             double priceOfSkumriq = double.Parse(Console.ReadLine());
             double priceOfCaca = double.Parse(Console.ReadLine());
@@ -22,8 +17,7 @@ namespace Fishland
             double priceOfPalamud = priceOfSkumriq + (priceOfSkumriq * 0.6);
             double priceOfSafrid = priceOfCaca + (priceOfCaca * 0.8);
 
-            //паламуд, сафрид и миди.
-
+          
             double palamud = kiloPalamut * priceOfPalamud;
             double safrid = kiloSafrid * priceOfSafrid;
             double midi = kiloMidi * priceOfMidi;
