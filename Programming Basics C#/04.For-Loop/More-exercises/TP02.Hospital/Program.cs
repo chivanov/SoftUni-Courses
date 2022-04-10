4168fb0c300631107e5c83e33983ebac11def639
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-10 10:52:46 +0200

    Add TP02 in For Loop More-exercises

---
+++ Programming Basics C#/04.For-Loop/More-exercises/TP02.Hospital/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02._Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int doctors = 7;
+            int yesPatient = 0;
+            int noPatient = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    if (noPatient > yesPatient)
+                    {
+                        doctors++;
+                    }
+                }
+
+                int currentPatients = int.Parse(Console.ReadLine());
+
+                if (doctors < currentPatients)
+                {
+                    yesPatient += doctors;
+                    noPatient += currentPatients - doctors;
+                }
+                else
+                {
+                    yesPatient += currentPatients;
+                }
+            }
+            Console.WriteLine($"Treated patients: {yesPatient}.");
+            Console.WriteLine($"Untreated patients: {noPatient}.");
+        }
+    }
+}
