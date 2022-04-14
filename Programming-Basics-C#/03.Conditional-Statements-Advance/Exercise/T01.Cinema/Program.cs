804da840caf15dc3a7f863eeca022a53e9bb44fc
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-14 14:59:59 +0200

    Add T01 in Conditional-Statements-Advance-Exercise

---
+++ Programming-Basics-C#/03.Conditional-Statements-Advance/Exercise/T01.Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01.SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string progection = Console.ReadLine();
+            int raws = int.Parse(Console.ReadLine());
+            int coloms = int.Parse(Console.ReadLine());
+
+            int totalSeats = raws * coloms;
+            double price = 0;
+
+            switch (progection)
+            {
+                case "Premiere":
+                    price = 12.00;
+                    break;
+                case "Normal":
+                    price = 7.50;
+                    break;
+                case "Discount":
+                    price = 5.00;
+                    break;
+
+            }
+            double income = totalSeats * price;
+            Console.WriteLine($"{income:F2} leva");
+        }
+    }
+}
