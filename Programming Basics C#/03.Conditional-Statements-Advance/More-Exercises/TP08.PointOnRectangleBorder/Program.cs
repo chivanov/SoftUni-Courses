ee01ec3f3204caaa71de2f839d1c18bb803dc13f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-17 09:27:18 +0200

    Add TP08 in Conditional Statement More Exercises

---
+++ Programming Basics C#/03.Conditional-Statements-Advance/More-Exercises/TP08.PointOnRectangleBorder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP08.PointOnRectangleBorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
