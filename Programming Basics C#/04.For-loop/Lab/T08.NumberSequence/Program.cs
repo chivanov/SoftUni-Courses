6c942039e69801f1bf42af84dcb8fcbd5313b4ec
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-20 07:01:28 +0200

    Add T08 in For Loop Lab

---
+++ Programming Basics C#/04.For-loop/Lab/T08.NumberSequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (maxNumber < number)
+                {
+                    maxNumber = number;
+                }
+                if (minNumber > number)
+                {
+                    minNumber = number;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
