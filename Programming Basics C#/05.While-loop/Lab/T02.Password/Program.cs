c213da47e4ecec97f8748d1b19d1a07035e31921
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-24 08:19:51 +0200

    Add T02 in While Loop Lab

---
+++ Programming Basics C#/05.While-loop/Lab/T02.Password/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            string passwordInput= Console.ReadLine ();
+
+            while (passwordInput != password)
+            {
+                passwordInput = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
