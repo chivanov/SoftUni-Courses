473a606437220039f866631ae0e250bef1df4cbe
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-24 08:19:22 +0200

    Add T01 in While Loop Lab

---
+++ Programming Basics C#/05.While-loop/Lab/T01.ReadText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T01.ReadText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            while (name != "Stop")
+            {
+                Console.WriteLine(name);
+                name = Console.ReadLine();
+            }
+        }
+    }
+}
