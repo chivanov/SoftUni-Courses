b7ee3f0bcba21767c58ec00e85f4b5ac20c7212d
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-25 17:21:55 +0200

    Add T06 in While Loop Lab

---
+++ Programming Basics C#/05.While-loop/Lab/T06.MaxNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int maxNumber = int.MinValue;
+            
+            while (input != "Stop")
+            {
+                int num = int.Parse(input);
+                if (maxNumber < num)
+                {
+                    maxNumber = num;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{maxNumber}");
+        }
+    }
+}
