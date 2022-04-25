49072d9bc33b5c22c6b0a698209b932fc47c263c
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-25 17:21:33 +0200

    Add T05 in While Loop Lab

---
+++ Programming Basics C#/05.While-loop/Lab/T05.AccountBalance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double sum = 0;
+
+            while (input != "NoMoreMoney")
+            {
+
+                double currentAmount = double.Parse(input);
+
+                if (currentAmount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                Console.WriteLine($"Increase: {currentAmount:f2}");
+                sum += currentAmount;
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
