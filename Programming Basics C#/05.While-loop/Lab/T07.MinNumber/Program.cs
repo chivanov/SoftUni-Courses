f463dc74af4df3c83013d5e8d343630118eefe8f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-25 17:22:16 +0200

    Add T07 in While Loop Lab

---
+++ Programming Basics C#/05.While-loop/Lab/T07.MinNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int minNumber = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int num = int.Parse(input);
+                if (minNumber > num)
+                {
+                    minNumber = num;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{minNumber}");
+        }
+    }
+}
