0884906e7c908c663641def4538383c1f3019202
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-27 07:51:13 +0200

    Add TP03 in While Loop More Exercises

---
+++ Programming Basics C#/05.While-loop/More-Exercises/TP03.StreamOfLetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP03.StreamOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
