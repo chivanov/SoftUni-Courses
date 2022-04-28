d8d8f5184246a7c03a37f08aa7036995ede537b0
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-28 09:58:46 +0200

    Add T01 in Nested Loops Exercise

---
+++ Programming Basics C#/06.Neted-loop/Exercise/T01.NumberPyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T01.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetNumber = int.Parse(Console.ReadLine());
+            int row = 1;
+            int currNum = 1;
+            bool isEqual = false;
+
+            while (!isEqual)
+            {
+                for (int i = 1; i <= row; i++)
+                {
+                    Console.Write($"{currNum} ");
+                    currNum++;
+                    if (currNum > targetNumber)
+                    {
+                        isEqual = true;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                row++;
+            }
+        }
+    }
+}
