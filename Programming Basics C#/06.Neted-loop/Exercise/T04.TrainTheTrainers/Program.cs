1dd797006b173de747e7f07313a1091d9c87e0e0
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-29 11:24:07 +0200

    Add T04 in Nested Loop Exercise

---
+++ Programming Basics C#/06.Neted-loop/Exercise/T04.TrainTheTrainers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int evaluatosNumber = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine ();
+            int gradeCount = 0;
+            double sumOfAllGrades = 0;
+
+            while (input != "Finish")
+            {
+                double sumOfGrades = 0;
+                for (int i = 1; i <= evaluatosNumber; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sumOfGrades += grade;
+                    gradeCount++;
+                    sumOfAllGrades += grade;
+                }
+
+                double averegeForCurrentPresentationn = sumOfGrades / evaluatosNumber;
+                Console.WriteLine($"{input} - {averegeForCurrentPresentationn:f2}.");
+
+                input = Console.ReadLine();
+            }
+            double averegeFinalGrade = sumOfAllGrades / gradeCount;
+            Console.WriteLine($"Student's final assessment is {averegeFinalGrade:f2}.");
+             
+        }
+    }
+}
