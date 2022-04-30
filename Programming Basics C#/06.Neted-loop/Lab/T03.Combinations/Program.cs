f934699fa67f53c00700e3ac08b5158dddf90004
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-04-30 16:39:48 +0200

    Add T03 in Nested Loop Lab

---
+++ Programming Basics C#/06.Neted-loop/Lab/T03.Combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T03.Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int validCombinationCounter = 0;
+            for (int x1 = 0; x1 <= number; x1++)
+            {
+                for (int x2 = 0; x2 <= number; x2++)
+                {
+                    for (int x3 = 0; x3 <= number; x3++)
+                    {
+                        if (x1 + x2 + x3 == number)
+                        {
+                            validCombinationCounter ++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"{validCombinationCounter}");
+        }
+    }
+}
