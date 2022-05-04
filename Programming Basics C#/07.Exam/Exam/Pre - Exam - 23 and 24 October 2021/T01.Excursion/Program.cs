d5015d83079919ab49967003061616f6fc46b7ce
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-04 06:42:37 +0200

    Add T01 in Exam

---
+++ Programming Basics C#/07.Exam/Exam/Pre - Exam - 23 and 24 October 2021/T01.Excursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T01.Excursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleNumber = int.Parse(Console.ReadLine());
+            int numberOfNights = int.Parse(Console.ReadLine());
+            int transportCards = int.Parse(Console.ReadLine());
+            int museumTickets = int.Parse(Console.ReadLine());
+
+            
+            int sumAccomodation = numberOfNights * 20;
+            double sumTransport = transportCards * 1.60;
+            int sumMuseum = museumTickets * 6;
+
+            double totalSum = (sumTransport + sumAccomodation + sumMuseum) * peopleNumber;
+            double finalSum = totalSum + (totalSum * 0.25);
+
+            Console.WriteLine($"{finalSum:f2}");
+
+
+
+        }
+    }
+}
