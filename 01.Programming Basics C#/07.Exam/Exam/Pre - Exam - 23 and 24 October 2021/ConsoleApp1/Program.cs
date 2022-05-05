2b2435c4943c3228f3841b11f579fedb5d68c18f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-05 07:35:31 +0200

    change file name

---
+++ 01.Programming Basics C#/07.Exam/Exam/Pre - Exam - 23 and 24 October 2021/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(num %5);
+            Console.WriteLine(num / 5);
+        }
+    }
+}
