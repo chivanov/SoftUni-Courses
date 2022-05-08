4dc6991516cc2f64ea711c38b8bf1aa9fae56bd6
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-08 08:13:50 +0200

    Add T05 in Data Type Exercise

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Exercise/T05.PrintPartOfASCII/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T05.PrintPartOfASCII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int code1 = int.Parse(Console.ReadLine());
+            int code2 = int.Parse(Console.ReadLine());
+
+
+
+            for (int symbol = code1; symbol <= code2; symbol++)
+            {
+                char currChar = Convert.ToChar(symbol);              
+
+                Console.Write($"{currChar} ");
+            }
+
+        }
+    }
+}
