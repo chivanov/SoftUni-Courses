2071872cffd554e40e8780b1bd2188c08b6a6397
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-09 07:49:30 +0200

    Add T09 in Data Type Exercise

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Exercise/T09.SpiceMustFlow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint yield = uint.Parse(Console.ReadLine());
+
+            int dayCounter = 0;
+
+            int totalSpise = 0;
+
+           
+        }
+    }
+}
