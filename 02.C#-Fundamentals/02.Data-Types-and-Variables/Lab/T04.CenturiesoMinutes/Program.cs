acda5f0666678f11940acb28228155da9c08ab3e
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-10 07:01:18 +0200

    Add T04 in Data Type Lab

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Lab/T04.CenturiesoMinutes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        byte centuries = byte.Parse(Console.ReadLine());
+        float years = (float)(centuries * 100);
+        int days = (int)(years * 365.2422);
+        int hours = (days * 24);
+        long minutes = (long)(hours * 60);
+
+        Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+    }
+}
