fd1e3acb12249faa27467fb2f480b4c3ecde2d81
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-10 07:00:36 +0200

    Add T02 in Data Type Lab

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Lab/T02.PoundsToDollars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        double pounds = double.Parse(Console.ReadLine());
+        decimal dolars = (decimal)(pounds * 1.31);
+
+        Console.WriteLine($"{dolars:f3}");
+
+    }
+}
