4ceb105b7b11c10fd3af5ca7e864b13f744c77ad
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-10 07:00:16 +0200

    Add T01 in Data Type Lab

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Lab/T01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int metres = int.Parse(Console.ReadLine());
+        decimal kilometres = (decimal)(metres / 1000.0);
+        Console.WriteLine($"{kilometres:f2}");
+    }
+}
