b517405faf04b8a17232a68601efe185ce2c85d9
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-11 06:35:09 +0200

    Add T06 in Data Type Lab

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Lab/T06.ReversedChars/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        char character1 = char.Parse(Console.ReadLine());
+        char character2 = char.Parse(Console.ReadLine());
+        char character3 = char.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{character3 } {character2 } {character1}");
+
+    }
+}
