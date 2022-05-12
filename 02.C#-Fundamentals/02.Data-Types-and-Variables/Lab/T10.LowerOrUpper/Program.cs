c44f34462dcbe9c3eeaea488dcd28cfe0d0b87a4
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-12 06:40:20 +0200

    Add T10 in Data Types Lab

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Lab/T10.LowerOrUpper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        char character = char.Parse(Console.ReadLine());
+        if (character >= 'a' && character <= 'z')
+
+        {
+            Console.WriteLine("lower-case");
+        }
+        else if (character >= 'A' && character <= 'Z')
+        {
+            Console.WriteLine("upper-case");
+        }
+
+    }
+}
