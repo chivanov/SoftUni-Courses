91d157d9fcb41431ffa35e5548abda61cebc6a54
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-12 06:39:56 +0200

    Add T09 in Data Types Lab

---
+++ 02.C#-Fundamentals/02.Data-Types-and-Variables/Lab/T09.CharsToString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+
+        char char1 = char.Parse(Console.ReadLine());
+        char char2 = char.Parse(Console.ReadLine());
+        char char3 = char.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{char1}{char2}{char3}");
+    }
+}
