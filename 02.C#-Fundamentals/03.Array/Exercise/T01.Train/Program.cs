8bfd40045d3d2b7515cc72dbbf813306b6eac300
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-13 06:42:13 +0200

    Add T01 in Array Exercise

---
+++ 02.C#-Fundamentals/03.Array/Exercise/T01.Train/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace T01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfVagons = int.Parse(Console.ReadLine());
+
+            int[] people = new int[numberOfVagons];
+
+            int sum = 0;
+
+            for (int i = 0; i < numberOfVagons; i++)
+            {
+
+                people[i] = int.Parse(Console.ReadLine());
+
+                sum += people[i];
+            }
+            Console.Write(String.Join(' ', people));
+            Console.WriteLine();
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
