308385d3e1ba6152d380a41893a8c8af0be65d55
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-13 06:43:48 +0200

    Add T05 in Array Exercise

---
+++ 02.C#-Fundamentals/03.Array/Exercise/T05.TopIntegers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] topIntArr = new int[arr.Length];
+            int topIntegerIndex = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isTopInteger = true;
+
+                int currNum = arr[i];
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int nextNum = arr[j];
+
+                    if (currNum <= nextNum)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+
+                if (isTopInteger)
+                {
+                    topIntArr[topIntegerIndex] = currNum;
+                    topIntegerIndex++;
+                }
+                
+            }
+
+            for (int i = 0; i < topIntegerIndex; i++)
+            {
+                Console.Write($"{topIntArr[i]} ");
+            }
+        }
+    }
+}
