32eb60a737e859c070cbe25f1c1b7ef6e1c6a390
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-16 06:42:32 +0200

    Add T07 in Array Lab

---
+++ 02.C#-Fundamentals/03.Array/Lab/T07.EqualArrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace T07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] arr2 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            bool isIdentical = true;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isIdentical = false;
+                    break;
+                }
+            }
+            int sum = 0;
+            foreach (int i in arr1)
+            {
+                sum += i;
+            }
+            if (isIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
