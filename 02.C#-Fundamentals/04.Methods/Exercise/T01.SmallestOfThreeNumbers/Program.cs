ed04d3d4f20f243a9ae208b6b7f1f33bdabd970a
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-17 06:43:39 +0200

    Add T01 in Methods Exercise

---
+++ 02.C#-Fundamentals/04.Methods/Exercise/T01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCoubt = 3;
+
+            PrintSmallestNumber(numberCoubt);
+        }
+
+        static void PrintSmallestNumber(int number)
+        {
+            int theSmall = int.MaxValue;
+            for (int i = 0; i < number; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < theSmall)
+                {
+                    theSmall = num;
+                }
+            }
+            Console.WriteLine(theSmall);    
+        }
+    }
+}
