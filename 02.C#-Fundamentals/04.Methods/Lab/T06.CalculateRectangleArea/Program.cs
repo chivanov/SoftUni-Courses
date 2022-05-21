960286228131c75d118fb122c228fcabe86477cb
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-21 07:14:17 +0200

    Add T06 in Methods Lab

---
+++ 02.C#-Fundamentals/04.Methods/Lab/T06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T06.CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double wigth = double.Parse(Console.ReadLine());
+            double hight = double.Parse(Console.ReadLine());
+            double rectangleArea = CalculateRectangleArea (wigth, hight);
+            Console.WriteLine(rectangleArea);
+
+        }
+
+        static double CalculateRectangleArea(double width, double hight)
+        {
+            return width * hight;
+        }
+    }
+}
