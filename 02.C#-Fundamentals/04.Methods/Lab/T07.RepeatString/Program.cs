4db30974fcc6564bdfe3b58eca26d005bf9a0c95
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-21 07:14:44 +0200

    Add T07 in Methods Lab

---
+++ 02.C#-Fundamentals/04.Methods/Lab/T07.RepeatString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace T07.RepeatString
+{
+    internal class Program
+    {
+        public static StringBuilder StringBuilder { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string stringInput = Console.ReadLine();
+            int repiet = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepietString(stringInput, repiet));
+        }
+
+        static string RepietString(string input, int repiets)
+        {
+            string newString = String.Empty;
+
+            for (int i = 0; i < repiets; i++)
+            {
+                newString += input;
+            }
+
+            return newString;
+        }
+
+    }
+}
