adb8459e4f395d77c8ed029632472800dd96bb55
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-22 07:24:23 +0200

    Add T07 in Lists Exercise

---
+++ 02.C#-Fundamentals/05.Lists/Exercise/TE07.AppendArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TE06.CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lists = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)               
+                .ToList();
+
+            List<string> newList = new List<string>();
+
+
+            for (int i = lists.Count-1; i >= 0; i--)
+            {
+                var currentList = lists[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in currentList)
+                {
+                    newList.Add(item);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', newList));
+        }
+    }
+}
