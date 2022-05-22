0f4826b180a41d96fc136ca533f175ea791b4f3e
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-22 07:21:24 +0200

    Add T03 in Lists Exercise

---
+++ 02.C#-Fundamentals/05.Lists/Exercise/TE03.HouseParty/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TE03.HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            List<string> partyMembers = new List<string>();
+            
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                List<string> input = Console.ReadLine().Split(' ').ToList();
+                string person = input[0];
+                string command = input[2];
+
+                if (partyMembers.Contains(person) && command == "going!")
+                {
+                    Console.WriteLine($"{person} is already in the list!"); 
+                }
+                else if (partyMembers.Contains(person) && command == "not")
+                {
+                    partyMembers.Remove(person);
+                }
+                else if (!partyMembers.Contains(person) && command == "not")
+                {
+                    Console.WriteLine($"{person} is not in the list!");
+                }
+                else
+                {
+                    partyMembers.Add(person);
+                }              
+
+            }
+
+            foreach (string person in partyMembers)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
