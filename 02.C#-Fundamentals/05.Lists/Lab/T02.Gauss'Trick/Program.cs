791e045c24a1b21fca60cd3725ff66754c7a3b32
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-23 06:21:15 +0200

    Add T02 in Lists Lab

---
+++ 02.C#-Fundamentals/05.Lists/Lab/T02.Gauss'Trick/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02.Gauss_Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> output = new List<int>();
+
+            for (int i = 0; i < numbers.Count/2; i++)
+            {
+                int newNumber = numbers[i] + numbers[numbers.Count - 1 - i];
+                output.Add(newNumber);
+
+            }
+                if (numbers.Count % 2 != 0)
+                {
+                    output.Add(numbers[numbers.Count / 2]);
+                }
+
+            Console.WriteLine(String.Join(' ', output));
+        }
+    }
+}
