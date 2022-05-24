74c6e79f6494d56257076591d530b0b21ed5b81e
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-24 07:00:26 +0200

    Add T04 in Lists Lab

---
+++ 02.C#-Fundamentals/05.Lists/Lab/T04.ListOfProducts/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T04.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfProducts = int.Parse(Console.ReadLine());
+            List<string> listOfProducts = new List<string>();
+            int counter = 0;
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                string product = Console.ReadLine();
+                listOfProducts.Add(product);
+                counter = i;
+            }
+                listOfProducts.Sort();
+
+            for (int i = 0; i < listOfProducts.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{listOfProducts[i]}");
+            }
+
+           
+        }
+    }
+}
