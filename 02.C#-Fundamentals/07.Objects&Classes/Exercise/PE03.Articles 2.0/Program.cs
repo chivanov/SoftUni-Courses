d792fb1fc77d0736e239f2931df3f47b31a35248
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-26 08:24:11 +0200

    Add T03 in Objects&Classes

---
+++ 02.C#-Fundamentals/07.Objects&Classes/Exercise/PE03.Articles 2.0/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace E_03._Articles_2._0
+{
+    internal class Program
+    {
+        class Article
+        {
+            public Article(string title, string content, string author)
+            {
+                Title = title;
+                Content = content;
+                Author = author;
+            }
+
+            public string Title { get; set; }
+            public string Content { get; set; }
+            public string Author { get; set; }
+
+            public override string ToString()
+            {
+                //Console.WriteLine() -> This method is being invoked
+                return $"{this.Title} - {this.Content}: {this.Author}";
+                //"{title} - {content}: {author}"
+            }
+        }
+        static void Main(string[] args)
+        {
+            int numOfArcticles = int.Parse(Console.ReadLine());
+            List<Article> list = new List<Article>();
+            for (int i = 1; i <= numOfArcticles; i++)
+            {
+                string[] currArcls = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string title = currArcls[0];
+                string content = currArcls[1];
+                string author = currArcls[2];
+
+
+                Article article = new Article(title, content, author);
+                list.Add(article);
+            }
+
+            string command = Console.ReadLine();
+            if (command == "title" || command == "content" || command == "author")
+            {
+                foreach (var article in list)
+                {
+                    Console.WriteLine(article);
+                }
+            }
+
+        }
+    }
+}
