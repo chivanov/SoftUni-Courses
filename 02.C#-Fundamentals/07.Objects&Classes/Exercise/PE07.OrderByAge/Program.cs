bebbf916825c96a2cccda1fc858b8593ba82f919
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-27 07:43:05 +0200

    Add P07 in Objects and Classes Exercise

---
+++ 02.C#-Fundamentals/07.Objects&Classes/Exercise/PE07.OrderByAge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PE07.OrderByAge
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {ID} is {Age} years old.";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+                string[] personInfo = input.Split();
+                Person currPerson = new Person();   
+                currPerson.Name = personInfo[0];
+                currPerson.ID = personInfo[1];
+                currPerson.Age = int.Parse(personInfo[2]); 
+
+                people.Add(currPerson);               
+
+            }
+
+            foreach (Person person in people.OrderBy(person => person.Age))
+            {
+                Console.WriteLine(person);
+            }
+        }       
+    }
+}
