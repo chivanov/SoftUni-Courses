3deebb624492388ceac6ba0d4b3e56cd0e3c99dd
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-28 08:33:37 +0200

    Add P01 in Objects and Classes Lab

---
+++ 02.C#-Fundamentals/07.Objects&Classes/Lab/PL01.RandomizeWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PL01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+                string currentWord = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = currentWord;
+            }
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
