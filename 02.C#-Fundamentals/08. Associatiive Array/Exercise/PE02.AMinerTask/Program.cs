b501e1e68e5810fbfc7db9b0741173a774101900
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-05-30 06:40:31 +0200

    Add P02 in Associatiive Array Exercise

---
+++ 02.C#-Fundamentals/08. Associatiive Array/Exercise/PE02.AMinerTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PE02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, int> resourses = new Dictionary<string, int>();  
+            while (command != "stop")
+            {
+                int quantity = int.Parse (Console.ReadLine());
+                if (resourses.ContainsKey(command))
+                {
+                    resourses[command] += quantity;
+                }
+                else 
+                { 
+                resourses.Add(command, quantity);   
+                }
+                
+                command = Console.ReadLine();
+            }
+
+            foreach (var pair in resourses)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
