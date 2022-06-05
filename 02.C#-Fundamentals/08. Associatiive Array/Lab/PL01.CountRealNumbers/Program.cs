ca1780fafac219bddc70da628b0784ba30ba93e5
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-05 09:52:15 +0200

    Add T01 in Associative Array Lab

---
+++ 02.C#-Fundamentals/08. Associatiive Array/Lab/PL01.CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PL01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            SortedDictionary<int, int> result = new SortedDictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (result.ContainsKey(number))
+                {
+                    result[number] += 1;
+                }
+                else
+                {
+                    result.Add(number, 1);
+                }
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
