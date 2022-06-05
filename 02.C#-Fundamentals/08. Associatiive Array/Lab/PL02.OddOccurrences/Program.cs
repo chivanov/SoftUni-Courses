8cda2e68907a005c976bc29120b429e27fb8d68e
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-05 09:52:34 +0200

    Add T02 in Associative Array Lab

---
+++ 02.C#-Fundamentals/08. Associatiive Array/Lab/PL02.OddOccurrences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PL02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+               string wordInLowerCase = word.ToLower();
+
+                if (count.ContainsKey(wordInLowerCase))
+                {
+                    count[wordInLowerCase] += 1;
+                }
+                else
+                {
+                    count.Add(wordInLowerCase, 1);
+                }
+            
+            }
+            foreach (var item in count)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+
+        }
+    }
+}
