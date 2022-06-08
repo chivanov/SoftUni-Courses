a7d14fa74427f28962604037de7b36f9e2f32b5f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-08 05:40:02 +0200

    Add P04 in Text Processing Exercise

---
+++ 02.C#-Fundamentals/09.Text processing/Exercise/PE04.CaesarCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace PE04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char currChar in text)
+            {
+                int currPosition = currChar;
+                currPosition += 3;
+                sb.Append((char)currPosition);
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
