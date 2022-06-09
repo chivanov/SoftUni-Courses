b850337254e8141728a2cbcd4e9fac209da9f89c
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-09 06:41:35 +0200

    Add P07 in Text Processing Exercise

---
+++ 02.C#-Fundamentals/09.Text processing/Exercise/PE07.StringExplosion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace PE07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            int power = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != '>')
+                {
+                    if (power == 0)
+                    {
+                        sb.Append(text[i]);
+                    }
+                    else
+                    {
+                        power--;
+                    }                    
+                }
+                else
+                {
+                    if (text[i] == '>')
+                    {
+                        sb.Append(text[i]);
+                    }
+
+                    char nextChar = text[i + 1];
+                    string powerString = nextChar.ToString();
+                    power = int.Parse(powerString)+power;
+                    power--;                   
+                    i++;
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
