8796db211f8d4944f667764449fb7d81bde75598
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-15 07:03:49 +0200

    Add P04 in Regex Exercise

---
+++ 02.C#-Fundamentals/10.Regex/Exercise/PE04.StarEnigma/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PE04.StarEnigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            //string decriptingPattern = @"[STARstar]+";
+
+            string decrPattern = @"\@(?<planetName>[A-Za-z]+)[^@\-!:>]:(?<population>\d+)[^@\-!:>]!(?<atackType>A|D)!->(\d+)";
+
+            for (int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();             
+
+                string decriptedMessage = DecriptMessage(input);
+                               
+
+                Match orderInfo = Regex.Match(decriptedMessage, decrPattern);
+                if (orderInfo.Success)
+                { 
+                string plannetName = orderInfo.Groups["plannetName"].Value; 
+
+                
+                }
+            }
+        }
+
+        private static string DecriptMessage(string input)
+        {
+            int decrKey = GetDecriptionKey(input);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char currCh in input)
+            {
+                char decriptrdCh = (char)(currCh - decrKey);
+
+                sb.Append(decriptrdCh);
+            }
+            return sb.ToString();   
+        }
+
+        static int GetDecriptionKey(string input)
+        {
+            string decriptingPattern = @"[star]{1}";
+            MatchCollection matches = Regex.Matches(input, decriptingPattern, RegexOptions.IgnoreCase);
+            return matches.Count;   
+           
+        }
+    }
+}
