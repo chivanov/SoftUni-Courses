d207555ce917bf9905cd58651ac9c81a67cb929d
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-18 06:35:53 +0200

    Add PL02 in Regex Lab

---
+++ 02.C#-Fundamentals/10.Regex/Lab/PL02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PL02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            var phoneMatches = matches.Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join (", ", phoneMatches));
+          
+        }
+    }
+}
