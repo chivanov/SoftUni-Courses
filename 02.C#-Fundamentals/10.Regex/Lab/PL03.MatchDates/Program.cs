dc7efe44670248740a0c45ad7139cd06ad8591cf
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-20 06:55:57 +0200

    Add P03 in Regex Lab

---
+++ 02.C#-Fundamentals/10.Regex/Lab/PL03.MatchDates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PL03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>[0-9]{2})(.|-|\/)(?<month>[A-Z][a-z]{2})(\1)(?<year>[0-9]{4})\b";
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}
