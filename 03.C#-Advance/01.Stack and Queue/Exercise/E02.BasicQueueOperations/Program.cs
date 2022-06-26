6e566c5403fe906259536215d8e30de2b61f7330
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-06-26 08:05:28 +0200

    Add E02 in Stack and Queue

---
+++ 03.C#-Advance/01.Stack and Queue/Exercise/E02.BasicQueueOperations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+              .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>();    
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+        }
+    }
+}
