e0ba66840e069c0f1539291a9c0c7b1ace2b5ef0
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-07-05 18:28:57 +0300

    Add E09 in Stack and Queue Exercise

---
+++ 03.C#-Advance/01.Stack and Queue/Exercise/E09.SimpleTextEditor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E09.SimpleTextEditor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder(); 
+            Stack<string> stack = new Stack<string>();  
+            for (int i = 0; i < n; i++)
+            {
+                string cmdInfo = Console.ReadLine();
+                if (cmdInfo.StartsWith('1'))
+                {
+                    stack.Push(sb.ToString());
+                    sb.Append(cmdInfo.Split()[1]);
+                }
+                else if (cmdInfo.StartsWith('2'))
+                {
+                    stack.Push(sb.ToString());
+                    int count = int.Parse(cmdInfo.Split()[1]);   
+                    sb.Remove(sb.Length-count, count);
+                }
+                else if (cmdInfo.StartsWith('3'))
+                {
+                    int index = int.Parse(cmdInfo.Split()[1]);
+                    Console.WriteLine(sb[index-1]);
+                }
+                else if (cmdInfo.StartsWith('4'))
+                {
+                    sb = new StringBuilder(stack.Pop());   
+                }
+            }
+        }
+    }
+}
