c64411c16a78b6a9d0f0799c8634f37bb58bcc37
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-07-06 13:52:05 +0300

    Add L03 in Stack and Queue Lab

---
+++ 03.C#-Advance/01.Stack and Queue/Lab/L03.SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L03.SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+            Stack<string> stack = new Stack<string>(input);
+
+            while (stack.Count > 1)
+            {
+                int num1 = int.Parse(stack.Pop());
+                string operant = stack.Pop();
+                int num2 = int.Parse(stack.Pop());
+
+                if (operant == "+")
+                {
+                   stack.Push((num1 + num2).ToString());
+                }
+                else if (operant == "-")
+                {
+                    stack.Push((num1 - num2).ToString());
+                }
+            }
+            Console.WriteLine(stack.Pop());
+          
+        }
+    }
+}
