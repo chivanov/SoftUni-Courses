b28e875cf048e60c027c17e87e0a8fef886e07f1
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-07-17 18:39:28 +0300

    Add L06 in Multidimensional Array Lab

---
+++ 03.C#-Advance/02.Multidimensional Arrays/Lab/L06.JaggedArrayModification/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace L06.JaggedArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[n][];  
+            for (int row = 0; row < n; row++)
+            {
+                int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                jagged[row] = arr;
+            }
+
+            string input = Console.ReadLine().ToLower();
+            while (input != "end")
+            {
+                string[] token = input.Split(' ');
+                int row = int.Parse(token[1]);
+                int col = int.Parse(token[2]);  
+                int value = int.Parse(token[3]);
+
+                if (token[0] == "add")
+                {
+                    if (IsValid(jagged, row, col))
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                    
+                }
+                else if (token[0] == "subtract")
+                {
+                    if (IsValid(jagged, row, col))
+                    {
+                        jagged[row][col] -= value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                }
+                
+                input = Console.ReadLine().ToLower();
+            }
+
+            foreach (var item in jagged)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+
+        private static bool IsValid(int[][] jagged, int row, int col)
+        {
+            if (row >= 0 && row < jagged.Length && col >= 0 && col < jagged.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
