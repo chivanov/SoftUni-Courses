4b6cc7f740683740999441bb4c071a05d1e7d8c6
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-07-23 20:45:09 +0300

    Add E04 in Sets and Dictionaries Exercise

---
+++ 03.C#-Advance/03.Sets and Dictionaries Advanced/Exercise/E04.EvenTimes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E04.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers.Add(num, 1);
+                }
+                else
+                {
+                    numbers[num]++;
+                }
+            }
+            foreach (var item in numbers)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
