016b067946bf8ca93b941ddf8f53c1a43d387dcf
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-07-28 08:29:08 +0300

    Add E05 in Steams, files and Dictionaries Exercise

---
+++ 03.C#-Advance/04.Streams, Files and Directories/Exercise/E04.DirectoryTraversal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            string[] files = Directory.GetFiles(inputFolderPath);
+            // take extencion of each file
+            Dictionary<string, List<FileInfo>> extensionsInfo = new Dictionary<string, List<FileInfo>>();
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string extension = fileInfo.Extension;
+                if (!extensionsInfo.ContainsKey(extension))
+                {
+                    extensionsInfo.Add(extension, new List<FileInfo>());
+                }
+                extensionsInfo[extension].Add(fileInfo);
+            }
+            StringBuilder sb = new StringBuilder(); 
+            foreach (var entry in extensionsInfo.OrderByDescending(entry => entry.Value.Count).ThenBy(entry => entry.Key))
+            {
+                string extension = entry.Key;
+                //Console.WriteLine(extension);
+                sb.AppendLine(extension);   
+                List<FileInfo> fileInfos = entry.Value;
+
+                fileInfos.OrderByDescending(File => File.Length);
+
+                foreach (var fileInfo in fileInfos)
+                {
+                    //Console.WriteLine($"--{fileInfo.Name} - {fileInfo.Length / 1024:f3}kb"); 
+                    sb.AppendLine($"--{fileInfo.Name} - {fileInfo.Length / 1024:f3}kb");
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string pathReport = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+            File.WriteAllText(pathReport, textContent);
+        }
+    }
+}
