e27c229a3c29ce1b59fc08c87f42d41c2a9ce589
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-08-03 06:28:15 +0300

    Add E07 in Functional programming Exercise

---
+++ 03.C#-Advance/05.Functional Programming/Exercise/E07.PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class PredicateForNames
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] names = Console.ReadLine().Split();
+
+        Func<string, int, bool> isInRange = (x,y) => (x.Length <= y);
+        List<string> result = new List<string>();   
+        foreach (var name in names)
+        {
+            if (isInRange(name, n))
+            {
+                Console.WriteLine(name);
+            }
+        };
+    }
+
+   
+}
+
