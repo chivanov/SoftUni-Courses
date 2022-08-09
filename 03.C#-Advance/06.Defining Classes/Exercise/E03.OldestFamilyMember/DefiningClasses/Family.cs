26d43342e5cb16c445ee143f568420f50efd1f1d
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-08-09 19:15:48 +0300

    Add E03 in Defining Classes Exercise

---
+++ 03.C#-Advance/06.Defining Classes/Exercise/E03.OldestFamilyMember/DefiningClasses/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> familyMembers;
+
+        public Family()
+        {
+            this.familyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.familyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            int maxAge = this.familyMembers.Max(m => m.Age);
+            return this.familyMembers.First(m => m.Age == maxAge);  
+        
+        }
+    }
+}
