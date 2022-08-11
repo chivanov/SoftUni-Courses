ddd1d0560e6743b87e6a9342426b245b86d8cdee
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-08-11 06:45:18 +0300

    Add E05 in defining classes exercise

---
+++ 03.C#-Advance/06.Defining Classes/Exercise/E04.OpinionPoll/DefiningClasses/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                string name = input.Split()[0];
+                int age = int.Parse(input.Split()[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            List<Person> overThirty = family.GetOverThirty();
+
+            foreach (var person in overThirty)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
