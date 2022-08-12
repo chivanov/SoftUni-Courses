f77c4891b6f348e33247b08979370366a6acb5bd
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-08-12 07:08:52 +0300

    Add E06 in defining classes exercise

---
+++ 03.C#-Advance/06.Defining Classes/Exercise/E05.Date Modifier/DefiningClasses/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
