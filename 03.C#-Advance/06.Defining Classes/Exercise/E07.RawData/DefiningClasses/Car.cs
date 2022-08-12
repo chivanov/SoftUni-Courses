f16f6a1c85f18892343a41255a8f9ab3467c4af9
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-08-12 07:09:44 +0300

    Add E07 in defining classes exercise

---
+++ 03.C#-Advance/06.Defining Classes/Exercise/E07.RawData/DefiningClasses/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private Tire[] tires;
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+
+        public Tire[] Tires
+        {
+            get => tires;
+            set
+            {
+                if (value.Length == 4)
+                {
+                    tires = value;
+                }
+            }
+        }
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Cargo = cargo;
+            this.Tires = tires;
+        }
+
+    }
+}
