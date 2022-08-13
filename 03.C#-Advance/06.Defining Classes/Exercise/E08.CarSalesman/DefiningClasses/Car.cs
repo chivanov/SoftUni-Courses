558152bf3900a73d0398964fbe656f33e83cd3f9
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-08-13 07:52:05 +0300

    Add E08 in defining classes exercise

---
+++ 03.C#-Advance/06.Defining Classes/Exercise/E08.CarSalesman/DefiningClasses/Car.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+
+        private string model;
+        private Engine engine;
+        private int? weight;
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int? Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = null;
+            this.Color = "n/a";
+        }
+        public Car(string model, Engine engine, int weight)
+         : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+        : this(model, engine)
+        {
+            this.Color = color;
+        }
+
+
+        public Car(string model, Engine engine, int weight, string color)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{this.Model}:");
+            sb.AppendLine($"  {this.Engine.Model}:");
+            sb.AppendLine($"   Power: {this.Engine.Power}");
+            if (this.Engine.Displacement == null)
+            {
+                sb.AppendLine("   Displacement: n/a");
+            }
+
+            else
+            {
+                sb.AppendLine($"   Displacement: {this.Engine.Displacement}");
+            }
+
+            sb.AppendLine($"   Efficiency: {this.Engine.Efficiency}");
+            if (this.Weight == null)
+            {
+                sb.AppendLine("  Weight: n/a");
+            }
+
+            else
+            {
+                sb.AppendLine($"  Weight: {this.Weight}");
+            }
+
+            sb.AppendLine($"  Color: {this.Color}");
+            return sb.ToString().Trim();
+        }
+    }
+}
