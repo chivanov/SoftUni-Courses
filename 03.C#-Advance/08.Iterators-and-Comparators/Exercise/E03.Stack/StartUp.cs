20b45fa1e2236e783fe957d9fa2c16c67bd72f48
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2022-09-25 18:24:20 +0300

    Add E03 in interators and comparators exercise

---
+++ 03.C#-Advance/08.Iterators-and-Comparators/Exercise/E03.Stack/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace E03.Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            string input;
+            while ((input=Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "Push")
+                {
+                    stack.Push(tokens.Skip(1).Select(e => e.Split(",").First()).ToArray());
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (AggregateException ae)
+                    {
+
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+            }
+
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
