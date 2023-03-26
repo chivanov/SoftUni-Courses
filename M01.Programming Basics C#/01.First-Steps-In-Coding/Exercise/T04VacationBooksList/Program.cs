3153cb98463372eeb68c8e0d102faef7bbbf4285
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-26 15:25:27 +0300

    Add module 1

---
+++ M01.Programming Basics C#/01.First-Steps-In-Coding/Exercise/T04VacationBooksList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T04VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            int numberOfBookPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int daysForReading = int.Parse(Console.ReadLine());
+
+            int readingTime = numberOfBookPages / pagesPerHour;
+            int hoursPerDay = readingTime / daysForReading;
+            Console.WriteLine(hoursPerDay);
+
+        }
+    }
+}
---
+++ M01.Programming Basics C#/01.First-Steps-In-Coding/Exercise/T05SuppliesForSchool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T05SuppliesForSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int countPens = int.Parse(Console.ReadLine());
+            int countMarkers = int.Parse(Console.ReadLine());
+            int cleaning = int.Parse(Console.ReadLine());
+            int percents = int.Parse(Console.ReadLine());
+
+            const double pensPrice = 5.80;
+            const double markersPrice = 7.20;
+            const double cleaningPrice = 1.20;
+
+            double pricePerPens = countPens * pensPrice;
+            double pricePerMarkers = countMarkers * markersPrice;
+            double pricePerCleaning = cleaning * cleaningPrice;
+            double percent = percents / 100.0;
+            double totalPrice = pricePerPens + pricePerMarkers + pricePerCleaning;
+            double finalPrice = totalPrice - (percent * totalPrice);
+
+            Console.WriteLine(finalPrice);
+
+        }
+    }
+}
