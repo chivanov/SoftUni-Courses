63d5ee984767a48f51cd1db5309fda624be18ac8
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-26 15:29:00 +0300

    dd Lab in first steps in coding

---
+++ M01.Programming Basics C#/01.First-Steps-In-Coding/Lab/T03RectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T03RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA = int.Parse (Console.ReadLine());
+            int sideB = int.Parse (Console.ReadLine());
+
+            int area = sideA * sideB;
+
+            Console.WriteLine(area);
+
+        }
+    }
+}
