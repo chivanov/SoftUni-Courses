040887a140a0dbf2764febd769e23d4c79a1e0b3
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-28 06:20:14 +0300

    Add file from T02 in inheritance

---
+++ M04.C#-OOP/L01.Inheritance/Lab/T02MultipleInheritance/Farm/StartUp.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Farm;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Puppy puppy = new Puppy();
+        puppy.Bark();
+        puppy.Eat();
+        puppy.Weep();
+    }
+}
