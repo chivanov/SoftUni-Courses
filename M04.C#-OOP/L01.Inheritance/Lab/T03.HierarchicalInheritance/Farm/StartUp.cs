3e14a81509213ca67e508d6bb16f1fb008b8a387
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-29 06:47:20 +0300

    Add T03 in inheritance lab

---
+++ M04.C#-OOP/L01.Inheritance/Lab/T03.HierarchicalInheritance/Farm/StartUp.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Farm;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+
+        Cat cat = new Cat();
+        cat.Eat();
+        cat.Meow();
+    }
+}
