30c9628067a9a358094a3bbdaac01e0befc45412
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-30 07:09:44 +0300

    Add T04 in inheritance lab

---
+++ M04.C#-OOP/L01.Inheritance/Lab/T04.RandomList/CustomRandomList/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+       
+        Random rdm = new Random();   
+        public string RemoveRandomElement()
+        {
+            if (Count == 0)
+            {
+                return null;
+            }
+
+            int index = rdm.Next(0, this.Count);
+            string text = this[index];
+            this.RemoveAt(index);
+            return text;
+        }
+    }
+}
