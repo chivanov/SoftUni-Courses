54ab4fccaedad83a79af5a7aea1650be88cf3a9c
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-30 07:10:04 +0300

    Add T04 in inheritance lab

---
+++ M04.C#-OOP/L01.Inheritance/Lab/T04.RandomList/CustomRandomList/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using CustomRandomList;
+
+public class StartUp
+
+{
+    static void Main(string[] args)
+    {
+        RandomList list = new RandomList();
+
+        list.Add("Pesho");
+        list.Add("Pesho2");
+        list.Add("Pesho3");
+        list.Add("Pesho4");
+        list.Add("Pesho5");
+        list.Add("Pesho6");
+        Console.WriteLine(list.RandomString());
+        Console.WriteLine(list.RandomString());
+        Console.WriteLine(list.RandomString());
+        Console.WriteLine(list.RandomString());
+        Console.WriteLine(list.RandomString());
+        Console.WriteLine(list.RandomString());
+        Console.WriteLine(list.RandomString());
+    }
+}
