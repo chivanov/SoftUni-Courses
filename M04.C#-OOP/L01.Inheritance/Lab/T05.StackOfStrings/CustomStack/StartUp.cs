b89d11c3543339bbbdb5926f7d17a7945f68cb02
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-03-31 06:56:30 +0300

    Add T05 in inheritance lab

---
+++ M04.C#-OOP/L01.Inheritance/Lab/T05.StackOfStrings/CustomStack/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new List<string>() { "1", "2", "3" });
+
+            Console.WriteLine(stack.IsEmpty());
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
