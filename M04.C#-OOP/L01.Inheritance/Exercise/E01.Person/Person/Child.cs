5f688d51d37e06cd2f5b8cfaac2cf563f7a61ab4
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-01 16:52:27 +0300

    Add E01 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E01.Person/Person/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+        }
+
+        public override int Age
+        {
+            get => base.Age;
+            set => base.Age = value; 
+        }
+
+    }
+}
