a8c65ce072eb9b8cf969177f0565a785c4184c7f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-01 16:52:46 +0300

    Add E01 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E01.Person/Person/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+		private string name;
+		private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+        public virtual int Age
+        {
+            get { return age; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException();
+                }
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
