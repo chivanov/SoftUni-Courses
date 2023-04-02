9dc78fa91e155138100520d0bd54f8f0f07a5357
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-02 15:31:14 +0300

    Add E01 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E02.Zoo/Zoo/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        private string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
