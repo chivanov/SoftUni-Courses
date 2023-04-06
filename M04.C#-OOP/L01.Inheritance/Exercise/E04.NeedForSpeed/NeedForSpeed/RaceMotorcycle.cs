9e796ca7e9d31e87e1b050d8048f30e1c144fbf4
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-06 18:32:29 +0300

    Add E04 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E04.NeedForSpeed/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public const double DefaultFuelConsumption = 8.0;
+        public RaceMotorcycle(int horsePower, double fuel) 
+            : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption
+            => DefaultFuelConsumption;
+    }
+}
