4c996490605d8951efc54f7c5959a469aac1c34a
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-06 18:33:17 +0300

    Add E04 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E04.NeedForSpeed/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }
+
+        public virtual double FuelConsumption
+            => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            if (this.Fuel - (kilometers * this.FuelConsumption) >= 0)
+            {
+                this.Fuel -= kilometers * this.FuelConsumption;
+            }
+        }
+    }
+}
