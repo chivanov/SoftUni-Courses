5524671285eb13ae9cd419fc099aece5a5964e9f
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-07 18:15:05 +0300

    Add E05 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E05.Restaurant/Restaurant/Beverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        public double Milliliters { get; set; }
+        public Beverage(string name, decimal price, double milliliters)
+            : base(name, price)
+        {
+            this.Milliliters = milliliters;
+        }
+    }
+}
