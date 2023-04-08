c551f604f4704d19e3be87a1d5d82004859eefaa
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-08 08:05:32 +0300

    Add E05 in inheritance exercise

---
+++ M04.C#-OOP/L01.Inheritance/Exercise/E05.Restaurant/Restaurant/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
