59653f69bde0d0109970873508b5b156a148cdc2
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-12 07:03:20 +0300

    Add E01 in encapsulation exercise

---
+++ M04.C#-OOP/L02.Encapsulation/Exercise/E01.ClassBoxData/Box/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {            
+
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double hight = double.Parse(Console.ReadLine());
+                Box box = new Box(length, width, hight);
+
+                Console.WriteLine(box);
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }            
+
+        }
+    }
+}
