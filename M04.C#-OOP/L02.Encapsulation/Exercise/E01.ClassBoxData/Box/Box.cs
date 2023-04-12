1b5cb22d07cb65cb8b4f4ba17df82b4f4fe76a8a
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-12 07:02:35 +0300

    Add E01 in encapsulation exercise

---
+++ M04.C#-OOP/L02.Encapsulation/Exercise/E01.ClassBoxData/Box/Box.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Box
+{
+    public class Box
+    {
+        private const double BoxMinValue = 0;
+       
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value <= BoxMinValue)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                else
+                {
+                    this.length = value;
+                }
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                if (value <= BoxMinValue)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                else
+                {
+                    this.width = value;
+                }
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value <= BoxMinValue)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                else
+                {
+                    this.height = value;
+                }
+            }
+        }
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double SurfaceArea()
+            => (2 * this.Length * this.Width) + (2 * this.Height * this.Length) + (2 * this.Width * this.Height);
+
+        public double LateralSurfaceArea()
+            => (2 * this.Height * this.Length) + (2 * this.Width * this.Height);
+
+        public double Volume()
+            => this.Length * this.Width * this.Height;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb
+                .AppendLine($"Surface Area - {SurfaceArea():f2}")
+                .AppendLine($"Lateral Surface Area - {LateralSurfaceArea():f2}")
+                .AppendLine($"Volume - {Volume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
