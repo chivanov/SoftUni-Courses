0b22b51416ae013aefed9c1932c5fd382c9d5f4b
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-14 12:05:17 +0300

    Add E03 in encapsulation exercise

---
+++ M04.C#-OOP/L02.Encapsulation/Exercise/E03.ShoppingSpree/ShoppingSpreeExercise/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpreeExercise
+{
+    public class Person
+    {
+        private string name;
+
+        private decimal money;
+        private List<Product> products;
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> Products => this.products;   
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get
+            {
+                return money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (Money - product.Cost < 0)
+            {
+                return false;
+            }
+            this.products.Add(product);
+            this.Money-= product.Cost; 
+
+            return true;
+        }
+
+
+    }
+}
