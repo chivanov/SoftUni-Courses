ce62e317a5ae7eb4a919487b302f16c50cf149f2
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-16 09:47:28 +0300

    Add E05 in encapsulation exercise

---
+++ M04.C#-OOP/L02.Encapsulation/Exercise/E05.FootballTeamGenerator/FootballTeamGenerator/Team.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballTeamGenerator
+{
+    public class Team
+    {
+        private string name;
+
+        private List<Player> players;
+        public Team(string name)
+        {
+            this.Name = name;
+            this.players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+                name = value;
+            }
+        }
+
+        public void AddPlayer(Player player)
+        => players.Add(player);
+
+        public bool RemovePlayer(string name)
+        {
+            Player player = players.FirstOrDefault(x => x.Name == name);
+            return players.Remove(player);
+        }
+
+        public int Stats
+            => players.Any() ? (int)Math.Round(this.players.Average(x => x.Stats)) : 0;
+
+    }
+}
