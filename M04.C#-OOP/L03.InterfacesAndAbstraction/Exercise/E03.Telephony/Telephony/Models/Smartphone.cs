63a01a27069eb3daeae48e16edf80839fa7a8ca0
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-29 08:18:55 +0300

    Add E03 in interfaces and abstractions exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E03.Telephony/Telephony/Models/Smartphone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telephony.Exeptions;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowseable
+    {
+
+        public string Call(string number)
+        {
+            if (!number.All(c => Char.IsDigit(c)))
+            {
+                throw new ArgumentException(ExeptionMessages.InvalidNumberExeption);
+            }
+            return number.Length > 7 ? $"Calling... {number}" : $"Dialing... {number}";
+        }
+        public string Browse(string url)
+        {
+            if (url.Any(c => char.IsDigit(c)))
+            {
+                throw new ArgumentException(ExeptionMessages.InvalidUrlExeption);
+            }
+            return $"Browsing: {url}!";
+        }
+
+      
+    }
+}
