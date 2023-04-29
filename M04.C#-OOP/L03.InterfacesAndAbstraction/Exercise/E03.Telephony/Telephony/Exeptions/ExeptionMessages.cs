9cfaa37f13db606e80c248ed8418f14df74e76b7
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-04-29 08:18:33 +0300

    Add E03 in interfaces and abstractions exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E03.Telephony/Telephony/Exeptions/ExeptionMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephony.Exeptions
+{
+    public static class ExeptionMessages
+    {
+        public static string InvalidNumberExeption = "Invalid number!";
+        public static string InvalidUrlExeption = "Invalid URL!";
+    }
+}
