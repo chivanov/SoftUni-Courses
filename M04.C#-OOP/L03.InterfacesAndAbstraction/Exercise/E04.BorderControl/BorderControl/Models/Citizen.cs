f3c33b13d4f8ef65f2719fcfe2365ab07f6bbe09
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-02 18:57:45 +0300

    Add E04 in interfaces a nd abstractions exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E04.BorderControl/BorderControl/Models/Citizen.cs
@@ -0,0 +1,18 @@
+﻿namespace BorderControl
+{
+    public class Citizen : IIdentifiable
+    {
+        public Citizen(string name, int age, string id)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Id { get; set; }  
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
