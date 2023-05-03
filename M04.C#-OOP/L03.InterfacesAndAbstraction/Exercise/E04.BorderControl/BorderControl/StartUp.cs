b8bcb25ce52c5e260e5d0b786efa10f71536db40
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-03 18:40:27 +0300

    Add E04 in interface and abstraction exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E04.BorderControl/BorderControl/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using BorderControl.Core;
+
+namespace BorderControl
+{    public class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
