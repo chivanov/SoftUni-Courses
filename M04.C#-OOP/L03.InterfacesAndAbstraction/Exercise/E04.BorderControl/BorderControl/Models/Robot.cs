30f6806ad43b745173be3477707d17db080123dd
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-03 18:40:05 +0300

    Add E04 in interface and abstraction exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E04.BorderControl/BorderControl/Models/Robot.cs
@@ -0,0 +1,15 @@
+﻿namespace BorderControl
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string model, string id)
+        {
+            this.Id = id;
+            this.Model = model;
+        }
+
+        public string Id { get; set; }
+
+        public string Model { get; private set; }
+    }
+}
