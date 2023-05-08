bf748ad0ea2b7d81c933b4463985859719a83067
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-08 10:47:58 +0300

    Add E05 in interface and abstraction exercose

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E05.BirthdayCelebrations/BirthdayCelebrations/Models/Citizen.cs
@@ -0,0 +1,25 @@
+﻿using BirthdayCelebrations.Contracts;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+        }
+
+        public string Id { get; set; }  
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+        public string Birthdate { get;  set; }
+
+        
+
+    }
+}
