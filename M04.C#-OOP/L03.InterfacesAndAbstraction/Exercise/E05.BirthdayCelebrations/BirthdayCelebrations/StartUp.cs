7799def576de93d0081a079b3dbd13ecaaee2705
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-11 19:09:52 +0300

    Add E05 in interface and abstraction exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E05.BirthdayCelebrations/BirthdayCelebrations/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using BorderControl.Core;
+
+namespace BirthdayCelebration
+{    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
