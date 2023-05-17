b75f69156a5ac980cf9429ed26621c9b72979e7e
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-17 18:43:35 +0300

    Add E06 in interface and abstraction exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E06.FoodShortage/FoodShortage/Models/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using BirthdayCelebrations.Contracts;
+using FoodShortage.Contracts;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifiable, IBirthable, IBuyer
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+        }
+
+        public string Id { get; set; }  
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+        public string Birthdate { get;  set; }
+
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 10;
+        }
+    }
+}
