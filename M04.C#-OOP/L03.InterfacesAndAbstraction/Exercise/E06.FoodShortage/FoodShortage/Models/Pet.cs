064ce164b730e1ab090f13b602e44023777d4a87
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-18 18:54:07 +0300

    Add in E06 in interface and abstraction exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E06.FoodShortage/FoodShortage/Models/Pet.cs
@@ -0,0 +1,21 @@
+﻿using BirthdayCelebrations.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCelebrations.Models
+{
+    public class Pet : IBirthable
+    {
+        public Pet(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; set; }    
+        public string Birthdate { get ; set;    }
+    }
+}
