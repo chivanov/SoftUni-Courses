1102420036619041a5ee8946024906fca378d7be
Chavdar Ivanov <102951895+chivanov@users.noreply.github.com>
2023-05-19 19:32:18 +0300

    Add E06 in interface and abstraction exercise

---
+++ M04.C#-OOP/L03.InterfacesAndAbstraction/Exercise/E06.FoodShortage/FoodShortage/Models/Rebel.cs
@@ -0,0 +1,29 @@
+﻿using FoodShortage.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShortage.Models
+{
+    public class Rebel : IBuyer
+    {
+        public Rebel(string name, int age, string group)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Group = group;
+        }
+        public string Name { get; }
+
+        public int Food { get; private set; }
+        public int Age { get; private set; }
+        public string Group { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 5;
+        }
+    }
+}
